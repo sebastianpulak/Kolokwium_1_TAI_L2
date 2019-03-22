c7ed353b34b1ec35d537fab6652bd3ca21f9b160
Sebastian Pulak <pulakosso@gmail.com>
2019-03-22 16:37:40 +0100

    done

---
+++ navbar.js
@@ -0,0 +1,5 @@
+let nav = '<nav><h1 class="text">Menu</h1>' +
+    '<a class="text" href="./index.html">Home </a>' +
+    '<a class="text" href="./blog.html">Blog</a> </nav>';
+let elem = document.querySelector(".nav-menu");
+elem.innerHTML = nav;
---
+++ script.js
@@ -0,0 +1,6 @@
+let button = document.querySelector('.button');
+let box = document.querySelector('.header');
+
+button.addEventListener('click', function () {
+    box.style.color= 'blue';
+});
